21555b0532ef211438d8c5c26f35c27db4926876
Olivia Garrett <ojg5g8@mail.missouri.edu>
2019-11-03 16:08:03 -0600

    scrape assignment part 2

--- highway.py
+++ highway.py
@@ -1,4 +1,17 @@
-#I started with pip3 install resquests then mechanize then Beautiful Soup in the command line. Then I went to this file.
-
+import csv
 import requests, mechanize
 from bs4 import BeautifulSoup
+
+csvfile = open('highway.csv', 'a')
+jail_writer = csv.writer(csvfile)
+
+#####2.get html
+url="https://www.mshp.dps.missouri.gov/HP68/SearchAction?searchDate=10/31/2017"
+
+br = mechanize.Browser()
+br.open(url)
+html = br.response().read()
+
+#####3.set up soup
+soup = BeautifulSoup(html, "html.parser")
+
