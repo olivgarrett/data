8ed591fc6f8a98408971ca7e173b93814b1b1fb5
Olivia Garrett <ojg5g8@mail.missouri.edu>
2019-11-12 15:13:26 -0600

    finsihed scraper

--- highway.py
+++ highway.py
@@ -3,15 +3,24 @@ import requests, mechanize
 from bs4 import BeautifulSoup
 
 csvfile = open('highway.csv', 'a')
-jail_writer = csv.writer(csvfile)
+highway = csv.writer(csvfile)
 
 #####2.get html
 url="https://www.mshp.dps.missouri.gov/HP68/SearchAction?searchDate=10/31/2017"
-
-br = mechanize.Browser()
-br.open(url)
-html = br.response().read()
+html = requests.get(url).content
 
 #####3.set up soup
 soup = BeautifulSoup(html, "html.parser")
 
+table = soup.find('table', {'class': 'accidentOutput'})
+rows = table.find_all('tr')
+
+for row in rows:
+
+	tds = row.find_all('td')
+
+	output = []
+	for td in tds:
+		output.append(td.text.strip())
+
+	highway.writerow(output)
